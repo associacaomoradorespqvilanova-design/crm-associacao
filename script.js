ba9c6e359e978d45bbdba5d7aacce25e74b38ebd
associacaomoradorespqvilanova-design <associacaomoradorespqvilanova@gmail.com>
2026-08-08 16:51:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const URL_API_GS = "https://script.google.com/macros/s/AKfycbwDlcThiZxRzUP6ruvYUWCzkrof6WC9N_4HpmmcEVNA3VJqkvOE6kDBs57X5EVW8iSdDw/exec"; 
+const URL_API_GS = "https://script.google.com/macros/s/AKfycbzUFLIii3X2mVZ2BYTsJhFWC95VfBzVZRYFVHS8__jgegjSC9rXnsBuuCu-KANqnnoXKw/exec"; 
 
 function fetchFromGS(acao, params = {}, signal) {
     return new Promise((resolve, reject) => {
