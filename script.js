097396b0d58ce53df8d2df6b4a47f9946d757ee0
associacaomoradorespqvilanova-design <associacaomoradorespqvilanova@gmail.com>
2026-08-14 10:26:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ============================================================
 // CONFIGURAÇÃO DA API
 // ============================================================
-const URL_API_GS = "https://script.google.com/macros/s/AKfycbwZ9WdiSeiuPU43UoLIs7t-l9s0vcqYIPqpfoZpwEyKSSjYsOVmrHmCWdXSbHc-RqfAtw/exec";
+const URL_API_GS = "https://script.google.com/macros/s/AKfycbxEa0M67wI3FUjMOfnOdPktPpxv3iuafxwZGZBFwVmDdbZaroobUgOr00lRiUcGtAepUw/exec";
 
 // ============================================================
 // GET VIA JSONP
